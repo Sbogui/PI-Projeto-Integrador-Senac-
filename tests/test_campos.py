2bc011cc19ff37952440e0529e5e992fb1464a3d
04000865064 <04000865064@senacrs.edu.br>
2026-06-17 20:18:18 -0300

    Adiciona testes automatizados de cadastro com pytest

---
+++ tests/test_campos.py
@@ -0,0 +1,30 @@
+def test_cadastro_sem_nome(client):
+    response = client.post(
+        "/cadastro",
+        data={
+            "nome": "",
+            "email": "teste@email.com",
+            "senha": "123456",
+            "confirmar_senha": "123456",
+            "tipo": "aluno"
+        },
+        follow_redirects=True
+    )
+
+    assert b"Preencha todos os campos" in response.data
+    
+    
+def test_cadastro_sem_confirmar_senha(client):
+    response = client.post(
+        "/cadastro",
+            data={
+            "nome": "Pietro",
+            "email": "teste@email.com",
+            "senha": "123456",
+            "confirmar_senha": "",
+            "tipo": "aluno"
+        },
+        follow_redirects=True
+    )
+
+    assert b"As senhas n\xc3\xa3o conferem" in response.data
